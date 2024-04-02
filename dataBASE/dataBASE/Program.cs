bfbf4651a725d191fbf4c7a2809182ee225ff6a6
flock-of-jays <wg7504c@gre.ac.uk>
2024-04-02 11:02:17 +0100

    fix: remove embedded solution inside dataBase/dataBase/dataBase

--- dataBASE/dataBASE/Program.cs
+++ dataBASE/dataBASE/Program.cs
@@ -8,9 +8,7 @@ namespace dataBASE
 {
     internal static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
